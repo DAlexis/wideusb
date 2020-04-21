3c9ce7111f09332fc8c9ef9f3ceee8ea1972eeaa
Aleksey Bulatov <AlexeyABulatov@yandex.ru>
2020-04-21 22:18:27 +0300

    Initial build system

---
+++ source/mcu/wideusb/build.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+set -e
+
+echo "Usage: ./build.sh <debug|release>. Debug is default"
+
+cfg="Debug"
+build_dir_prefix="./build"
+build_dir=$build_dir_prefix"/debug"
+source_dir_from_build_dir="../../"
+
+if [ "$1" == "release" ];
+then
+    cfg="Release"
+    build_dir=$build_dir_prefix"/release"
+fi
+
+mkdir -p $build_dir
+(
+    cd $build_dir 
+    cmake -DCMAKE_BUILD_TYPE=${cfg} -DCMAKE_TOOLCHAIN_FILE=${source_dir_from_build_dir}/stm32f407.cmake ${source_dir_from_build_dir} 
+    make -j `nproc`
+)
---
+++ source/mcu/wideusb/cpp/test.cpp
@@ -0,0 +1,11 @@
+class Test
+{
+public:
+    Test()
+    {
+        int i = 0;
+        i += 1;
+    }
+};
+
+Test t;
